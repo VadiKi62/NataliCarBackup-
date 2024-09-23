61e317baedee6c7d35807802e55f3a0ad49653ca
Natalia Kirieieva <nataliakireewa@gmail.com>
2024-09-23 23:18:41 +0100

    resolving orders refetching5

--- utils/database.js
+++ utils/database.js
@@ -1,25 +1,70 @@
 import mongoose from "mongoose";
 
-let isConnected = false; // track the connection
+const MONGODB_URI = process.env.MONGODB_URI;
 
-export const connectToDB = async () => {
-  mongoose.set("strictQuery", true);
+if (!MONGODB_URI) {
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env.local"
+  );
+}
 
-  if (isConnected) {
-    console.log("MongoDB is already connected");
-    return;
+/**
+ * Global is used here to maintain a cached connection across hot reloads
+ * in development. This prevents connections growing exponentially
+ * during API Route usage.
+ */
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+export async function connectToDB() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+      dbName: "Car",
+    };
+
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+      return mongoose;
+    });
   }
 
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      dbName: "Car",
-      // useNewUrlParser: true,
-    });
-
-    isConnected = true;
-
-    console.log("MongoDB connected");
-  } catch (error) {
-    console.log(error);
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
   }
-};
+
+  return cached.conn;
+}
+
+// let isConnected = false; // track the connection
+
+// export const connectToDB = async () => {
+//   mongoose.set("strictQuery", true);
+
+//   if (isConnected) {
+//     console.log("MongoDB is already connected");
+//     return;
+//   }
+
+//   try {
+//     await mongoose.connect(process.env.MONGODB_URI, {
+//       dbName: "Car",
+//       // useNewUrlParser: true,
+//     });
+
+//     isConnected = true;
+
+//     console.log("MongoDB connected");
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
