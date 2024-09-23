755696b14dcbbcf993d96e57081ba968076ad203
Natalia Kirieieva <nataliakireewa@gmail.com>
2024-09-23 22:39:11 +0100

    resolving orders refetching4

--- app/api/order/all/route.js
+++ app/api/order/all/route.js
@@ -15,9 +15,9 @@ export const GET = async (req, res) => {
         "Content-Type": "application/json",
         "Cache-Control":
           "no-store, no-cache, must-revalidate, proxy-revalidate",
-        Pragma: "no-cache",
-        Expires: "0",
-        "Surrogate-Control": "no-store",
+        // Pragma: "no-cache",
+        // Expires: "0",
+        // "Surrogate-Control": "no-store",
       },
     });
   } catch (error) {
