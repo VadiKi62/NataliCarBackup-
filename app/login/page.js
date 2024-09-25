2f201245df5e03782968f72d172adab14da991b6
Natalia Kirieieva <nataliakireewa@gmail.com>
2024-09-25 15:41:18 +0100

    admin page2

--- app/login/page.js
+++ app/login/page.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "@app/components/loginForm/login.module.css";
+import styles from "@app/components/LoginForm/login.module.css";
 import LoginForm from "@app/components/LoginForm/loginForm";
 
 const LoginPage = () => {
