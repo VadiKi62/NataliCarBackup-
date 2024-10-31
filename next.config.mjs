8c18e290524dda94961471632586f76009517522
Natalia Kirieieva <nataliakireewa@gmail.com>
2024-10-31 08:46:00 +0000

    new url

--- next.config.mjs
+++ next.config.mjs
@@ -6,9 +6,7 @@ const nextConfig = {
   images: { unoptimized: true },
   env: {
     NEXT_LOCAL_API_BASE_URL: "http://localhost:3000",
-    // NEXT_PUBLIC_API_BASE_URL: "https://car-jade.vercel.app/",
-    NEXT_PUBLIC_API_BASE_URL:
-      "https://car-git-new-context-nataliakiis-projects.vercel.app/",
+    NEXT_PUBLIC_API_BASE_URL: "https://car-jade.vercel.app/",
   },
 };
 
