89d32c98baed4b716068af23ea8fb2fb964dec30
Natalia Kirieieva <nataliakireewa@gmail.com>
2025-08-11 12:17:34 +0300

    new commit2

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -40,7 +40,8 @@ import {
 } from "@mui/material";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+// import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFnsV3";
 import { ru } from "date-fns/locale";
 
 const StyledBox = styled(Box)(({ theme }) => ({
