f7a2da2b9484c9ce76ef238ec190167966e5bbc9
VadiKi62 <ntf.elcor@gmail.com>
2025-10-01 19:49:52 +0300

    Изменен расчет  цены аренды в модальном окне BookingModal

--- app/api/order/calcTotalPrice/route.js
+++ app/api/order/calcTotalPrice/route.js
@@ -7,31 +7,31 @@ export async function POST(request) {
     await connectToDB();
     const { carNumber, rentalStartDate, rentalEndDate } = await request.json();
     if (!carNumber || !rentalStartDate || !rentalEndDate) {
-      return new Response(
-        JSON.stringify({ message: "Missing parameters" }),
-        { status: 400, headers: { "Content-Type": "application/json" } }
-      );
+      return new Response(JSON.stringify({ message: "Missing parameters" }), {
+        status: 400,
+        headers: { "Content-Type": "application/json" },
+      });
     }
     const car = await Car.findOne({ carNumber });
     if (!car) {
-      return new Response(
-        JSON.stringify({ message: "Car not found" }),
-        { status: 404, headers: { "Content-Type": "application/json" } }
-      );
+      return new Response(JSON.stringify({ message: "Car not found" }), {
+        status: 404,
+        headers: { "Content-Type": "application/json" },
+      });
     }
     const totalPrice = await car.calculateTotalRentalPricePerDay(
       dayjs(rentalStartDate),
       dayjs(rentalEndDate)
     );
     const days = dayjs(rentalEndDate).diff(dayjs(rentalStartDate), "day") + 1;
-    return new Response(
-      JSON.stringify({ totalPrice, days }),
-      { status: 200, headers: { "Content-Type": "application/json" } }
-    );
+    return new Response(JSON.stringify({ totalPrice, days }), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
   } catch (error) {
-    return new Response(
-      JSON.stringify({ message: error.message }),
-      { status: 500, headers: { "Content-Type": "application/json" } }
-    );
+    return new Response(JSON.stringify({ message: error.message }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
   }
 }
