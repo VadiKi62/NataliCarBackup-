5c9e89d20d1305d999a0eaaca6d2427734417521
VadiKi62 <ntf.elcor@gmail.com>
2025-10-26 22:37:15 +0200

    new comit

--- app/layout.js
+++ app/layout.js
@@ -19,7 +19,7 @@ export default function RootLayout({ children }) {
         <meta name="theme-color" content="#ffffff" />
         <meta name="msapplication-navbutton-color" content="#ffffff" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        {/* <meta name="color-scheme" content="light" /> */}
+        {/* <meta  name="color-scheme"  content="light" /> */}
         <meta name="color-scheme" content="only light" />
       </head>
       <body>
